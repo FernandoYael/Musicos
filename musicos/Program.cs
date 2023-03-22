0d232c8c2d005b8e74aa300de76ac426919d3fc9
Demonear <126305470+FernandoYael@users.noreply.github.com>
2023-03-22 19:30:23 +0000

    1

--- musicos/Program.cs
+++ musicos/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Musico
+{
+    public string Nombre {get; set;}
+    public Musico(string nombre)
+    {
+        Nombre=nombre;
+    }
+    public void Saludar()
+    {
+        Console.WriteLine("Hola");
+    }
+    public virtual void Tocar()
+    {
+        Console.WriteLine($"{Nombre} esta tocando su Instrumento");
+    }
+}
+
+class Baterista
+{
+    //Bateria
+}
+
+class Bajista
+{
+    //Bajista
+}
+
+//class Guitarrista
+//{
+    //Guitarra
+//}
+
+//class Vocalista
+//{
+    //Microfono
+//}
+
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Musico L = new Musico("L");
+        L.Saludar();
+        L.Tocar();
+    }
+}
